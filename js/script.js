c9803334504495efb7725b0b22be068cbba16f8f
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-17 15:40:14 +0200

    added palindrome check function

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ const resultEl = document.querySelector('.alert');
 
 btn.addEventListener('click', function () {
     //inputEl.
-    let num = parseInt(inputEl.value);
+    let string = String(inputEl.value);
+    console.log(string);
     let alertColor;
-    if (!isNaN(num)) {
-        if (num % 2 === 1) {
-            num = num + 1;
-        }
+    if (isNaN(string) && string !== '') {
+        resultEl.classList.remove('alert-danger');
         alertColor = 'alert-success';
-
-        resultEl.innerHTML = num;
+        resultEl.innerHTML = (isPalindrome(string)) ? 'la stringa è palindroma' : 'la stringa non è palindroma';
     } else {
         console.log('errore')
+        resultEl.classList.remove('alert-success')
         alertColor = 'alert-danger'
-        resultEl.innerHTML = `inserire un numero`
+        resultEl.innerHTML = `inserire una stringa di caratteri in formato valido`
     }
     resultEl.classList.add(alertColor);
     resultEl.classList.remove('d-none');
+
 });
---
+++ js/utilities.js
@@ -0,0 +1,34 @@
+/**
+ * given two integers, returns a pseudo-randomic between the two (included)
+ * @param {Integer} min 
+ * @param {Integer} max 
+ * @returns {Integer}
+ */
+function rndInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * given an integer return true if even, false if odd
+ * @param {Integer} number 
+ * @returns {Boolean}
+ */
+function isEven(number) {
+    return !(number % 2);
+}
+
+/**
+ * return true if palindrome, false elseway
+ * @param {string} [string  to analiaze if palindrome] 
+ * @returns {boolean} 
+ */
+function isPalindrome(string) {
+    //avoid conflicts for spaces or Capital case 
+    string = string.replace(/\s/g, '').toLowerCase();
+    for (let i = 0; i <= (string.length / 2); i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
